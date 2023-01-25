6973d711f3f4ed08900cb186c65a38670ed642b3
griffinhamskin <113440113+griffinhamskin@users.noreply.github.com>
2023-01-24 23:41:37 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
+import random
+
 WIN_WIDTH= 800
 WIN_HEIGHT= 640
 TILESIZE = 95
 FPS = 60
 
 ENEMY_SPEED = 2
-ENEMY_LAYER=4
-PLAYER_LAYER=2
+ENEMY_LAYER=2
+PLAYER_LAYER=4
 BLOCK_LAYER=1
 PLAYER_SPEED = 3
 GROUND_LAYER = 1
@@ -16,20 +18,24 @@ BLUE = (0,0,255)
 GREEN = (0,255,0)
 WHITE =(255,255,255)
 
+x = ['E','B']
+
+x1=random.choice(x)
+
 tilemap = [
-    'BBBBBBBBBBBBBBBBBBBB',
-    'B.E................B',
-    'B...P.BB...........B',
-    'B..BBB.............B',
-    'B..................B',
-    'B..................B',
-    'B..................B',
-    'B..................B',
-    'B...E........BB....B',
-    'B.............B....B',
-    'B...........B......B',
-    'B..................B',
-    'B..................B',
-    'B..................B',
-    'BBBBBBBBBBBBBBBBBBBB',
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
+    'B.R......RRR.................B',
+    'B...P.RR........RRRRR........B',
+    'B..RRR.......................B',
+    'B.......RRRRRRR..............B',
+    'B............................B',
+    'B.....RRRRRRRRR..............B',
+    'B............................B',
+    'B...R........RR..............B',
+    'B.............R..............B',
+    'B...........R.....RRR........B',
+    'B......R.....................B',
+    'B............................B',
+    'B............................B',
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
 ]
